6ef21411f970f8e9f33eaea3bdaca8b4e0c1f93c
Jardel Cruz <jardelcruz6@gmail.com>
2025-11-29 14:33:22 -0300

    criando o compnente Home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { ListarLivros } from './listar-livros/listar-livros';
 import { AdicionarLivro } from './adicionar-livro/adicionar-livro';
 import { DeletarLivro } from './deletar-livro/deletar-livro';
 import { AtualizarLivro } from './atualizar-livro/atualizar-livro';
+import { Home } from './home/home';
 
 export const routes: Routes = [
-  { path: '', component: ListarLivros },
+  { path: '', component: Home },
+  { path: 'listarLivros', component: ListarLivros },
   { path: 'adicionarLivros', component: AdicionarLivro },
   { path: 'deletarLivros', component: DeletarLivro },
   { path: 'atualizarLivro', component: AtualizarLivro },
