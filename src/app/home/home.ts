93d964ef4e7d188660b69b5458b8b7e78aa5b23b
Jardel Cruz <jardelcruz6@gmail.com>
2025-11-29 16:54:24 -0300

    Home construida com o header e os icons de login, favoritos e carrinho

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Header } from '../header/header';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+   standalone: true,
+  imports: [CommonModule, Header],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
